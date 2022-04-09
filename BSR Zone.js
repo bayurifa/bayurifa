b190325625098121ffb35ec7ab1708b316a0e614
bayurifa <91443870+bayurifa@users.noreply.github.com>
2022-04-10 03:26:43 +0700

    Sticky Bottom BSR Zone.js

---
+++ BSR Zone.js
@@ -0,0 +1,10 @@
+/* Get all elements with class="dc-close" */
+var closebtns = document.getElementsByClassName("dc-close");
+var i;
+
+/* Loop through the elements, and hide the parent, when clicked on */
+for (i = 0; i < closebtns.length; i++) {
+  closebtns[i].addEventListener("click", function() {
+    this.parentElement.style.display = 'none';
+  });
+}
